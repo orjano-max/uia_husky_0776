6028c14b833128dc242c08e6f870389824a6e9db
Lars-33 <larsmuggerud@proton.me>
2023-03-23 12:12:01 +0100

    aa

--- husky_group/launch/husky2.launch.py
+++ husky_group/launch/husky2.launch.py
@@ -12,57 +12,93 @@ from launch_ros.actions import PushRosNamespace
 def generate_launch_description():
 
     # Get LIDAR parameters
-    lidar_params = PathJoinSubstitution(
-        [FindPackageShare('husky_group'),
-        'params',
-        'ouster_lidar.yaml'],
+    lidar_params = GroupAction(
+        actions=[
+            PushRosNamespace('husky'),
+            PathJoinSubstitution(
+                [FindPackageShare('husky_group'),
+                'params',
+                'ouster_lidar.yaml'],
+            )
+        ]
     )
 
-    localization_params = PathJoinSubstitution(
-        [FindPackageShare('husky_group'),
-        'params',
-        'localization.yaml'],
+    localization_params = GroupAction(
+        actions=[
+            PushRosNamespace('husky'),
+            PathJoinSubstitution(
+                [FindPackageShare('husky_group'),
+                'params',
+                'localization.yaml'],
+            ),
+        ]
     )
 
-    urdf_extras_path = PathJoinSubstitution(
+    urdf_extras_path = GroupAction(
+        actions=[
+            PushRosNamespace('husky'),
+            PathJoinSubstitution(
                 [FindPackageShare("husky_group"), "urdf", "husky_urdf_extras.urdf"]
+            ),
+        ]
     )
 
-    config_husky_velocity_controller = PathJoinSubstitution(
-        [FindPackageShare("husky_group"),
-        "params",
-        "control.yaml"],
+    config_husky_velocity_controller = GroupAction(
+        actions=[
+            PushRosNamespace('husky'),  
+            PathJoinSubstitution(
+                [FindPackageShare("husky_group"),
+                "params",
+                "control.yaml"],
+            ),
+        ]
     )
-    
+            
     # Launch the husky robot using the husky_uia uia_master_husky repo
-    launch_husky_base = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(PathJoinSubstitution(
-        [FindPackageShare("husky_base"), 'launch', 'base_launch.py'])),
-        launch_arguments ={
-            "urdf_extras" : urdf_extras_path,
-            "localization_params" : localization_params,
-            "config_husky_velocity_controller" : config_husky_velocity_controller,
-        }.items()
+    launch_husky_base = GroupAction(
+        actions=[
+            PushRosNamespace('husky'),  
+            IncludeLaunchDescription(
+                PythonLaunchDescriptionSource(PathJoinSubstitution(
+                [FindPackageShare("husky_base"), 'launch', 'base_launch.py'])),
+                launch_arguments ={
+                    "urdf_extras" : urdf_extras_path,
+                    "localization_params" : localization_params,
+                    "config_husky_velocity_controller" : config_husky_velocity_controller,
+                }.items()
+            ),
+        ]
     )
-
 
     #Launch the UM7 IMU
-    node_um7_imu = Node(
-        package="um7",
-        executable="um7_node",
-        output="screen",
+    node_um7_imu = GroupAction(
+        actions=[
+            PushRosNamespace('husky'),  
+            Node(
+                package="um7",
+                executable="um7_node",
+                output="screen",
+            ),
+        ]
     )
 
 
     #Launch the LIDAR
-    launch_ouster_lidar = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(PathJoinSubstitution(
-        [FindPackageShare("ros2_ouster"), 'launch', 'os1_launch.py'])),
-        launch_arguments ={
-            "params_file" : lidar_params
-        }.items()
+    launch_ouster_lidar = GroupAction(
+        actions=[
+            PushRosNamespace('husky'), 
+            IncludeLaunchDescription(
+                PythonLaunchDescriptionSource(
+                    PathJoinSubstitution([
+                        FindPackageShare("ros2_ouster"), 'launch', 'os1_launch.py'
+                        ])
+                ),
+                launch_arguments ={
+                    "params_file" : lidar_params
+                }.items()
+            ),
+        ]
     )
-
 
     #Launch the pointcloud to laserscan package
     node_pointcloud_to_laserscan = GroupAction(
