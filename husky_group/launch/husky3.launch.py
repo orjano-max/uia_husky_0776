3a14bbaf84c3a3a3a8c566712c18205e276f9290
Lars-33 <larsmuggerud@proton.me>
2023-03-23 12:15:50 +0100

    aa

--- husky_group/launch/husky3.launch.py
+++ husky_group/launch/husky3.launch.py
@@ -44,53 +44,12 @@ def generate_launch_description():
     )
 
 
-    #Launch the UM7 IMU
-    node_um7_imu = Node(
-        package="um7",
-        executable="um7_node",
-        output="screen",
-    )
-
-
-    #Launch the LIDAR
-    launch_ouster_lidar = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(PathJoinSubstitution(
-        [FindPackageShare("ros2_ouster"), 'launch', 'os1_launch.py'])),
-        launch_arguments ={
-            "params_file" : lidar_params
-        }.items()
-    )
-
-
-    #Launch the pointcloud to laserscan package
-    node_pointcloud_to_laserscan =  Node(
-    	name="pointcloud_to_laserscan",
-        package="pointcloud_to_laserscan",
-        remappings=[('cloud_in', '/points')],
-        executable="pointcloud_to_laserscan_node",
-        parameters=[{
-        'target_frame': 'base_laser',
-        'transform_tolerance': 0.01,
-        'min_height': -0.50,
-        'max_height': 0.2,
-        'angle_min': -math.pi,  # -M_PI/2
-        'angle_max': math.pi,  # M_PI/2
-        'angle_increment': 0.0087,  # M_PI/360.0
-        'scan_time': 0.3333,
-        'range_min': 0.8,
-        'range_max': 120.0,
-        'use_inf': True,
-        'inf_epsilon': 1.0,
-        }]
-    )
 
 
     ld = LaunchDescription()
 
     # Launch pointcloud to laserscan, imu and lidar
-    ld.add_action(node_pointcloud_to_laserscan)
-    ld.add_action(node_um7_imu)
-    ld.add_action(launch_ouster_lidar)
+
 
     # Launch Husky UGV
     ld.add_action(launch_husky_base)
