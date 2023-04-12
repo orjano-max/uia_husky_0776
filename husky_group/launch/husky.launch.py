6060e921f81d4408a0803e8ea5d38d035806722b
Orjan <orjano18@uia.no>
2023-04-12 16:39:28 +0200

    fdsaf

--- husky_group/launch/husky.launch.py
+++ husky_group/launch/husky.launch.py
@@ -18,7 +18,7 @@ def generate_launch_description():
 
     print("URDF PATH: ",urdf_extras_path)
 
-    os.environ["CPR_URDF_EXTRAS"] = str(urdf_extras_path)
+    #os.environ["CPR_URDF_EXTRAS"] = str(urdf_extras_path)
     os.environ["HUSKY_TOP_PLATE_ENABLED"] = "false"
     os.environ["HUSKY_SERIAL_PORT"] = "/dev/ttyUSB1"
 
